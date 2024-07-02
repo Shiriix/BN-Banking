cb3b4f2863da990fcf18c8fd7e38ed5bb720334f
unknown <solomonpipi1@gmail.com>
2024-07-02 16:39:50 +0100

    added balance chart to homepage UI

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -1,19 +1,33 @@
 "use client";
 import { Doughnut } from "react-chartjs-2";
+import { Chart, ArcElement } from "chart.js";
+Chart.register(ArcElement);
 
 const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
   const data = {
     datasets: [
       {
         label: "Orders Count",
-        data: [1250, 600, 23211],
+        data: [1250, 600, 2322],
         backgroundColor: ["#0747b7", "#2265d8", "#2f91fa"],
       },
     ],
     labels: ["Bank 1", "Bank 2", "Bank 3"],
   };
 
-  return <Doughnut data={data} />;
+  return (
+    <Doughnut
+      data={data}
+      options={{
+        cutout: "60%",
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      }}
+    />
+  );
 };
 
 export default DoughnutChart;
