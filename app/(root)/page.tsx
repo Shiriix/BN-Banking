7214e821186fe1fd6abe9b966243f161caa94042
unknown <solomonpipi1@gmail.com>
2024-07-03 15:27:20 +0100

    added right sidebar and created bank card component to display bank account information

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Sol" };
+  const loggedIn = {
+    firstName: "Sol",
+    lastName: "P",
+    email: "shiri@gmail.com",
+  };
 
   return (
     <section className="home">
@@ -22,7 +27,14 @@ const Home = () => {
             totalCurrentBalance={420.69}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transations={[]}
+        banks={[{ currentBalance: 1800 }, { currentBalance: 1337 }]}
+      />
     </section>
   );
 };
